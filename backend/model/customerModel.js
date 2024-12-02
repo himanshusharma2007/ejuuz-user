6cc9e336769c1b7fe0d54753b5571e285ff7854d
himanshusharma2007 <sharmahimanshu6478@gmail.com>
2024-12-02 11:53:06 +0530

    changes in customer model /himanshu

--- backend/model/customerModel.js
+++ backend/model/customerModel.js
@@ -4,21 +4,23 @@ const bcrypt = require('bcryptjs');
 const customerSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
     trim: true
   },
   email: {
     type: String,
-    required: true,
+  
     unique: true,
     lowercase: true
   },
   password: {
     type: String,
-    required: true,
     select: false
   },
-  phoneNumber: String,
+  phoneNumber: {
+    type:String,
+    required: true,
+    trim:true
+  },
   address: {
     street: String,
     city: String,
