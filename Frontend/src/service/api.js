c33aaa107a0b5024e343b5fa3f865c70c2b80d72
Dhairya jain <dhairyajain2617@gmail.com>
2024-12-18 14:37:00 +0530

    api base url changed /dhairya

--- Frontend/src/service/api.js
+++ Frontend/src/service/api.js
@@ -3,7 +3,8 @@ import { Alert } from "react-native";
 import { resetRoot } from "./RootNavigation";
 
 const api = axios.create({
-  baseURL: "http://192.168.1.14:3000/api",
+  baseURL: "https://ejuuz-user.onrender.com/api",
+  // baseURL: "http://192.168.1.14:3000/api",
   withCredentials: true,
 });
 
